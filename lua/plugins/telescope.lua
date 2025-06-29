0d5a9a20c97127ccb1bb888f2ba8041cda4ce0e1
Gabriel Prado de Moraes <gpradodemoraes@outlook.com>
2025-06-29 20:05:28 -0300

    adicionado um shortcut para o telescope find files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,6 +15,7 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
+	  vim.keymap.set("n", "<C-a>", builtin.find_files, {})
       vim.keymap.set("n", "<C-p>", builtin.oldfiles, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       local actions = require("telescope.actions")
